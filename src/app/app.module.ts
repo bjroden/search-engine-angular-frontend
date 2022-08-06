3fa10970e711e5dc0a8e6c5b2940190ca80afe96
bjroden <bjroden@uark.edu>
2022-08-06 16:47:26 -0500

    Error messages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 
 import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
+import { ToastModule } from 'primeng/toast';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SearchResultsComponent } from './components/search-results/search-results.component';
 import { SearchInputComponent } from './components/search-input/search-input.component';
+import { MessageService } from 'primeng/api';
 
 @NgModule({
   declarations: [
@@ -19,13 +22,15 @@ import { SearchInputComponent } from './components/search-input/search-input.com
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
     HttpClientModule,
     AppRoutingModule,
     InputTextModule,
-    ButtonModule
+    ButtonModule,
+    ToastModule
   ],
-  providers: [],
+  providers: [MessageService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/services/query-service.service.ts
+++ src/app/services/query-service.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Subject } from 'rxjs';
 import { SearchResult } from '../dataModels/SearchResult';
+import { MessageService } from 'primeng/api';
 
 @Injectable({
   providedIn: 'root'
@@ -9,20 +10,36 @@ import { SearchResult } from '../dataModels/SearchResult';
 export class QueryService {
   searchResults$: Subject<SearchResult[]> = new Subject();
 
-  constructor(private http: HttpClient) { }
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService
+  ) { }
 
   makeSearch(query: string): void {
     this.http.get<SearchResult[]>(`/api?query=${query}`).subscribe({
       next: results => this.sendSearchResults(results),
-      error: error => console.log(error)
+      error: error => this.logError(error.message)
     });
   }
 
   sendSearchResults(results: SearchResult[]): void {
-    this.searchResults$.next(results);
+    if (results.length <= 0) {
+      this.logError("No results for query");
+    }
+    else {
+      this.searchResults$.next(results);
+    }
   }
 
   getSearchResults(): Subject<SearchResult[]> {
     return this.searchResults$;
+  }
+
+  logError(message: string): void {
+    this.messageService.add({
+      severity: 'error',
+      summary: "Error",
+      detail: message
+    })
   }
 }
